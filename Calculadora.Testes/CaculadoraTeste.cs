14a0d24654b8945217d87c354794c5eac2f6e426
Louise Rodrigues <nutrilouiserodrigues@gmail.com>
2022-08-05 17:31:12 -0300

    Initial commit

---
+++ Calculadora.Testes/CaculadoraTeste.cs
@@ -0,0 +1,29 @@
+namespace Calculadora.Testes
+{
+   
+    public class CaculadoraTeste
+    {
+        [Fact]
+        public void Somar()
+        {
+            Assert.Equal(20, Calculadora.Somar(10, 10));
+        }
+        [Fact]
+        public void Subtrair()
+        {
+            Assert.Equal(0, Calculadora.Subtrair(10, 10));
+        }
+
+        [Fact]
+        public void Multiplicar()
+        {
+            Assert.Equal(100, Calculadora.Somar(10, 10));
+        }
+       
+        [Fact]
+        public void Dividir()
+        {
+            Assert.Equal(1, Calculadora.Dividir(10, 10));
+        }
+    }
+}
